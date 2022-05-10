b39e2fe5d76ca910b4c0a5a04ddec668c2510a12
YashDaga17 <dagayash17@gmail.com>
2022-05-10 14:22:45 +0530

    Modifying Changes

--- ReadMe.md
+++
@@ -1,5 +0,0 @@
-# Watch Design
-
-###  The Application is developed using JavaScript using HarmonyOS.
-
-![image](https://user-images.githubusercontent.com/72146802/163391504-2506eba9-b234-44da-81df-f36a734c3019.png)
---
+++ entry/src/ohosTest/java/com/example/smart_watch_one/ExampleOhosTest.java
@@ -0,0 +1,14 @@
+package com.example.smart_watch_one;
+
+import ohos.aafwk.ability.delegation.AbilityDelegatorRegistry;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExampleOhosTest {
+    @Test
+    public void testBundleName() {
+        final String actualBundleName = AbilityDelegatorRegistry.getArguments().getTestBundleName();
+        assertEquals("com.example.mywatch", actualBundleName);
+    }
+}
